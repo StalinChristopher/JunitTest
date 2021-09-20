c4a563b2f98cd6619fd75af592e1a80c8df41f13
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-21 00:46:29 +0530

    Added UC3 email validation

--- src/main/java/com/yml/userregistration/UserRegistration.java
+++ src/main/java/com/yml/userregistration/UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	final String NAME = "^[A-Z]{1}[a-zA-Z]{2,}";
+	final String EMAIL = "^[0-9a-zA-Z]+([.+_\\-][0-9a-zA-Z]*)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
 	
 	public boolean validateFirstName(String firstName) {
 		return Pattern.matches(NAME, firstName);
@@ -11,5 +12,9 @@ public class UserRegistration {
 	
 	public boolean validateLastName(String lastName) {
 		return Pattern.matches(NAME, lastName);
+	}
+	
+	public boolean validateEmail(String email) {
+		return Pattern.matches(EMAIL, email);
 	}
 }
