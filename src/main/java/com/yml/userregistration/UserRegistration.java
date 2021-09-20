d6a58aa6647f6f6b99e07984de9b08fd5e26bfb4
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-21 01:13:20 +0530

    Added password rule 1 validation test cases

--- src/main/java/com/yml/userregistration/UserRegistration.java
+++ src/main/java/com/yml/userregistration/UserRegistration.java
@@ -6,6 +6,7 @@ public class UserRegistration {
 	final String NAME = "^[A-Z]{1}[a-zA-Z]{2,}";
 	final String EMAIL = "^[0-9a-zA-Z]+([.+_\\-][0-9a-zA-Z]*)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
 	final String PHONE = "[0-9]{2}[\\s][0-9]{10}";
+	final String PASSWORD = "[0-9a-zA-Z!@#$%^&*]{8,}";
 	
 	public boolean validateFirstName(String firstName) {
 		return Pattern.matches(NAME, firstName);
@@ -21,5 +22,9 @@ public class UserRegistration {
 	
 	public boolean validatePhone(String phone) {
 		return Pattern.matches(PHONE, phone);
+	}
+	
+	public boolean validatePassword(String password) {
+		return Pattern.matches(PASSWORD, password);
 	}
 }
