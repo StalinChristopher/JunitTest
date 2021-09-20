ed8aae2823f79046a42b28a80bb38ea2307fbcda
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-21 00:33:55 +0530

    Added UC2 lastName validation

--- src/test/java/com/yml/userregistrationtest/UserRegistrationTest.java
+++ src/test/java/com/yml/userregistrationtest/UserRegistrationTest.java
@@ -36,4 +36,34 @@ public class UserRegistrationTest {
 		boolean result = userReg.validateFirstName("ab");
 		Assert.assertTrue(result);
 	}
+	
+	/**
+	 * Test case for correct name with minimum 3 characters starting with capital letter
+	 */
+	@Test
+	public void lasttname_ifcorrect_returntrue() {
+		UserRegistration userReg = new UserRegistration();
+		boolean result = userReg.validateLastName("Dsouza");
+		Assert.assertTrue(result);
+	}
+	
+	/**
+	 * Test case for lastname with less than 3 characters, it will return false
+	 */
+	@Test
+	public void lasstname_haslessthan3characters_returnfalse() {
+		UserRegistration userReg = new UserRegistration();
+		boolean result = userReg.validateLastName("ab");
+		Assert.assertTrue(result);
+	}
+	
+	/**
+	 * Test case for lastname with name not starting with capital latter
+	 */
+	@Test
+	public void lastname_ifdoesnotstartwithcapital_returnfalse() {
+		UserRegistration userReg = new UserRegistration();
+		boolean result = userReg.validateLastName("afasfas");
+		Assert.assertTrue(result);
+	}
 }
