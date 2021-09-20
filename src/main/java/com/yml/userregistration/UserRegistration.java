bcbb6a3e9ae0c0284f68e3d48732e1f82a4b58e7
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-21 00:28:45 +0530

    Added UC1 test cases for valid and invalid first name

--- src/main/java/com/yml/userregistration/UserRegistration.java
+++ src/main/java/com/yml/userregistration/UserRegistration.java
@@ -1,5 +1,11 @@
 package com.yml.userregistration;
 
-public class UserRegistration {
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+	final String NAME = "^[A-Z]{1}[a-zA-Z]{2,}";
+	
+	public boolean validateFirstName(String firstName) {
+		return Pattern.matches(NAME, firstName);
+	}
 }
--- src/test/java/com/yml/userregistrationtest/UserRegistrationTest.java
+++ src/test/java/com/yml/userregistrationtest/UserRegistrationTest.java
@@ -1,5 +1,39 @@
 package com.yml.userregistrationtest;
 
-public class UserRegistrationTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+import com.yml.userregistration.UserRegistration;
+
+public class UserRegistrationTest {
+	
+	/**
+	 * Test case for correct name with minimum 3 characters starting with capital letter
+	 */
+	@Test
+	public void firstname_ifcorrect_returntrue() {
+		UserRegistration userReg = new UserRegistration();
+		boolean result = userReg.validateFirstName("Stalin");
+		Assert.assertTrue(result);
+	}
+	
+	/**
+	 * Test case for firstname with less than 3 characters, it will return false
+	 */
+	@Test
+	public void firstname_haslessthan3characters_returnfalse() {
+		UserRegistration userReg = new UserRegistration();
+		boolean result = userReg.validateFirstName("ab");
+		Assert.assertTrue(result);
+	}
+	
+	/**
+	 * Test case for firstname with name not starting with capital latter
+	 */
+	@Test
+	public void firstname_ifdoesnotstartwithcapital_returnfalse() {
+		UserRegistration userReg = new UserRegistration();
+		boolean result = userReg.validateFirstName("ab");
+		Assert.assertTrue(result);
+	}
 }
